e118c5dd3b83d7848e946d834406f9fc2c6cd9bf
Dan G <dano9573@gmail.com>
2018-01-23 12:19:05 -0800

    First challenge upload

---
+++ ChallengeOne/ChallengeOne/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeOne
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            var age = ageTextBox.Text;
+            var money = moneyTextBox.Text;
+
+            string result = "At " + age + "years old, I would have expected you to have more than " + money + " in your pocket.";
+
+            resultLabel.Text = result;
+            
+        }
+    }
+}
