e3f59298ba10db19030220158a18e997a3afdc43
Dan G <dano9573@gmail.com>
2018-02-01 15:04:34 -0800

    Challenge Simple Darts

---
+++ DWGChallengeSimpleDarts/DWGChallengeSimpleDarts/Score.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Darts;
+
+namespace DWGChallengeSimpleDarts
+{
+    public class Score
+    {
+        public static void ScoreDart(Player player, Dart dart)
+        {
+            int score = 0;
+
+            //Full dart board inner ring / outer ring
+            if (dart.IsTriple) score = dart.Score * 3;
+            else if (dart.IsDouble) score = dart.Score * 2;
+            else score = dart.Score;
+
+            //Bullseye inner ring / outer ring
+            if (dart.IsTriple && dart.Score == 0) score = 50;
+            else if (dart.Score == 0) score = 25;
+
+            player.Score += score; 
+
+
+        }
+
+
+    }
+}
